dd4eaf88cceb147c71636b27d3234d40587e165f
Ehb Teng <ehb@diginido.com>
2020-04-17 10:27:51 -0700

    Add files via upload

---
+++ AutoLayoutExercise/Auto Layout Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout Exercise
+//
+//  Created by Conan The Librarian on 4/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
